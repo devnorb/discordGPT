d0edc8c5c530d677cd2b795303e3d1972145ac17
norbsdev <127006485+norbsdev0@users.noreply.github.com>
2023-03-05 15:45:32 -0800

    v1.0.1

--- index.js
+++ index.js
@@ -2,25 +2,50 @@ const { Configuration, OpenAIApi } = require("openai");
 
 class discordGPT {
   constructor(options) {
+    this.textAccuracy = options.textAccuracy;
     this.apiKey = options.apiKey;
   }
 
   async generateText(message) {
-    const configuration = new Configuration({
-      apiKey: this.apiKey,
-    });
-    const openai = new OpenAIApi(configuration);
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      max_tokens: 2000,
-      messages: [
-        {
-          role: "user",
-          content: message,
-        },
-      ],
-    });
-    return completion.data.choices[0].message.content;
+    if (typeof options.textAccuracy !== "string") {
+      throw new TypeError(
+        "[discordGPT]: options.textAccuracy should be type String!"
+      );
+    } else if (options.textAccuracy > 2 || options.textAccuracy < 0) {
+      throw new RangeError(
+        "[discordGPT]: options.textAccuracy should be less than or equal to 0 and more than or equal to 2!"
+      );
+    } else {
+      const configuration = new Configuration({
+        apiKey: this.apiKey,
+      });
+      try {
+        const openai = new OpenAIApi(configuration);
+      } catch (e) {
+        if (e.response.status == 401) {
+          function APIError(errorMsg) {
+            this.name = "APIError";
+            this.message = errorMsg;
+          }
+          throw new APIError(`[discordGPT]: The API key provided is invalid!`);
+        } else {
+          throw new APIError(
+            `[discordGPT]: An error occurred. \n Details: ` + e
+          );
+        }
+      }
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: this.options.textAccuracy,
+        messages: [
+          {
+            role: "user",
+            content: message,
+          },
+        ],
+      });
+      return completion.data.choices[0].message.content;
+    }
   }
 }
 
